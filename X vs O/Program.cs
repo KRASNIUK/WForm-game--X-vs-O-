a23eddd7f458a07ce4ba17c8abb8d789ca73f882
KRASNIUK <58930287+KRASNIUK@users.noreply.github.com>
2021-01-03 04:52:37 +0200

    Console Write

--- X vs O/Program.cs
+++ X vs O/Program.cs
@@ -17,6 +17,8 @@ namespace X_vs_O
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Menu());
+
+            Console.WriteLine("dsc");
         }
     }
 }
